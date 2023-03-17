c3655e86e60efed8ec12c430d6901b0e01228a4b
Polushin Dima <dima2004polushin2004@gmail.com>
2023-03-17 21:40:39 +0600

    #backend start commit

---
+++ backend/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from "mongoose";
+import cors from 'cors';
+import config from "./config";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const run = async () => {
+  mongoose.set('strictQuery', false);
+  await mongoose.connect(config.db);
+
+  app.listen(port, () => {
+    console.log('Server ok: ', port);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch(console.error);
