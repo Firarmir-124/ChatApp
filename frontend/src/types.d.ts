ad0e8f7857f6780172b92721eaf178a7823a8e3e
Polushin Dima <dima2004polushin2004@gmail.com>
2023-03-17 21:38:32 +0600

    #frontend start commit

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,40 @@
+export interface RegisterMutation {
+  username: string;
+  password: string;
+  displayName: string;
+  avatar: File | null
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  token: string;
+  role: string;
+  displayName: string;
+  avatar: string | null
+}
+
+export interface RegisterResponse {
+  message: string;
+  user: User;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    }
+  },
+  name: string;
+  _name: string;
+}
+
+export interface LoginMutation {
+  username: string;
+  password: string;
+}
+
+export interface GlobalError {
+  error: string;
+}
