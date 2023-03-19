1c7e86be536f0642d86231cfa96cef14d63a6e02
Polushin Dima <dima2004polushin2004@gmail.com>
2023-03-19 23:20:42 +0600

    #frontend создал контейнер Home и сделал верстку

--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import Layout from "../../components/Layout/Layout";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  List,
+  Paper,
+  TextField
+} from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
 const Home = () => {
   return (
     <Layout>
-      123
+      <Container>
+        <Grid padding='10px' marginTop='10px' bgcolor='#12273c' container>
+          <Grid marginRight='10px' xs={3} item>
+            <Paper sx={{mb: '5px', p: '10px', bgcolor: '#2c3c4d'}} elevation={3}>
+              <List style={{ width: '100%', background: '#fff' }}>
+                Клиенты !
+              </List>
+            </Paper>
+          </Grid>
+          <Grid
+            sx={{
+              height: '500px',
+              overflowY: 'scroll',
+              '&::-webkit-scrollbar': {width:0}
+            }}
+            xs
+            item
+          >
+            <Paper sx={{px: '5px', bgcolor: '#2c3c4d'}} elevation={4}>
+              <List sx={{ width: '100%' }}>
+                Сообщения !
+              </List>
+            </Paper>
+          </Grid>
+        </Grid>
+
+        <Paper sx={{mt: '10px'}} elevation={3}>
+          <Box component='form'>
+            <TextField
+              type='text'
+              InputProps={{endAdornment: <Button variant="contained" endIcon={<SendIcon />}>Send</Button>}}
+              fullWidth
+            />
+          </Box>
+        </Paper>
+      </Container>
     </Layout>
   );
 };
