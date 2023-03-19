cb4349df6e46a49ede85c33984507db9c10953e6
Polushin Dima <dima2004polushin2004@gmail.com>
2023-03-19 23:38:22 +0600

    #frontend подключил ws к index

--- backend/index.ts
+++ backend/index.ts
@@ -3,13 +3,17 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import config from "./config";
 import usersRouter from "./routes/users";
+import chatRouter from "./routes/chat";
+import expressWs from "express-ws";
 
 const app = express();
 const port = 8000;
 
+expressWs(app);
 app.use(cors());
 app.use(express.json());
 app.use('/users', usersRouter);
+app.use(chatRouter);
 
 const run = async () => {
   mongoose.set('strictQuery', false);
